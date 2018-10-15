695a9ae464b9f49509bd96213d6f2d6daeb225b5
Percy Grunwald <percy.grunwald@gmail.com>
2018-10-15 20:42:54 +0800

    Ensure that the Endpoint is configured properly for releases

--- config/release.exs
+++ config/release.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :phoenix14_base, :env, (System.get_env("ENV") || "prod") |> String.to_atom()
 
-config :phoenix14_base, Phoenix14Base.Endpoint,
+config :phoenix14_base, Phoenix14BaseWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true
 
